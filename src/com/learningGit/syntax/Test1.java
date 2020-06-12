aaa0ee444e14d8788e2cbe73588ed24853c907f7
Carliz21 <carmenlizferrufino@gmail.com>
2020-06-11 22:32:01 -0400

    second commit

--- src/com/learningGit/syntax/Test1.java
+++ src/com/learningGit/syntax/Test1.java
@@ -3,6 +3,8 @@ package com.learningGit.syntax;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Learning git");
+		System.out.println("Making a change");
+		
 	}
 
 }
