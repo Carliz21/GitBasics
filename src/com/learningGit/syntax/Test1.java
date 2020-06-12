6f73c8fda6fa6d976eb13d4089a4de0aa19ad092
Carliz21 <carmenlizferrufino@gmail.com>
2020-06-11 22:25:04 -0400

    first commit

---
+++ src/com/learningGit/syntax/Test1.java
@@ -0,0 +1,8 @@
+package com.learningGit.syntax;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Learning git");
+	}
+
+}
--- src/com/syntax/learningGit/Test2.java
+++
@@ -1,5 +0,0 @@
-package com.syntax.learningGit;
-
-public class Test2 {
-
-}
